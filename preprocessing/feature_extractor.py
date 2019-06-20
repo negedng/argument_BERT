0cf8b8d638ea86ada095c70a594fdebd49a606b5
Gergely Németh <neged.ng@gmail.com>
2019-06-20 15:53:48 +0100

    add some argue features

---
+++ preprocessing/feature_extractor.py
@@ -0,0 +1,56 @@
+import nltk
+import numpy as np
+import pandas as pd
+import gensim
+from keras.preprocessing.sequence import pad_sequences
+
+
+WORD2WEC_EMBEDDING_FILE = '/root/input/GoogleNews-vectors-negative300.bin.gz'
+
+def get_propositions(dataset):
+    """Parse propositions
+    dataset: the original dataframe
+    Output:
+        propositionSet: list of the propositions of the arg1
+        parsedPropositions: parsed prop. in the arg1
+    """
+    
+    propositionSet = list(set(dataset['arg1']))
+    parsedPropositions = list()
+
+    for proposition in propositionSet:
+        words = nltk.tokenize.word_tokenize(proposition)
+        parsedPropositions.append(nltk.pos_tag(words))
+
+    return propositionSet, parsedPropositions
+    
+
+def word_vector_feature(dataset, propositionSet, parsedPropositions):
+    """Add word2vec feature to the dataset
+    """
+
+    word2VecModel = gensim.models.KeyedVectors.load_word2vec_format(WORD2WEC_EMBEDDING_FILE, binary=True)
+
+    wordVectorFeature = list()
+    feature_vector = np.zeros(300)
+
+    for proposition in parsedPropositions:
+        propositionVector = list()
+        for word in proposition:
+            if word[0] in word2VecModel.vocab:
+                feature_vector = word2VecModel[word[0]]
+
+            propositionVector.append(feature_vector)
+
+        wordVectorFeature.append(propositionVector)
+
+    wordVectorFeature = np.array(wordVectorFeature)
+    wordVectorFeature = pad_sequences(wordVectorFeature, value=0, padding='post', dtype=float)
+
+    wordVectorFrame = pd.DataFrame({"arg1": propositionSet, "vector1": wordVectorFeature.tolist()})
+    dataset = pd.merge(dataset, wordVectorFrame, on='arg1')
+    wordVectorFrame = wordVectorFrame.rename(columns={'arg1':'arg2', "vector1":"vector2"})
+    dataset = pd.merge(dataset, wordVectorFrame, on='arg2')
+
+    return dataset
+    
---
+++ preprocessing/model_builder.py
@@ -0,0 +1,15 @@
+import feature_extractor
+import timeit
+
+def argue_feature_build(dataset):
+    """Build feature to match ArguE features
+    """
+    propositionSet, parsedPropositions = feature_extractor.get_propositions(dataset)
+
+    print("-----1. Feature: EXTRACTING WORD VECTORS-----")
+    start_time = timeit.default_timer()
+    dataset = feature_extractor.word_vector_feature(dataset, propositionSet, parsedPropositions)
+    elapsed = timeit.default_timer() - start_time
+    print(elapsed)
+
+    return dataset
