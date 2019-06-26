f2399a8e2baac5c19b38f57c02e827df28939dbe
Gergely Németh <neged.ng@gmail.com>
2019-06-26 17:46:32 +0100

    add other ArguE features to feature_extractor

--- preprocessing/feature_extractor.py
+++ preprocessing/feature_extractor.py
@@ -1,11 +1,23 @@
+####
+#
+# Some features based on Tobias Milz's ArguE project. 
+# See more: 
+#
+####
+
 import nltk
 import numpy as np
 import pandas as pd
 import gensim
 from keras.preprocessing.sequence import pad_sequences as kp_pad_sequences
+from sklearn.preprocessing import LabelBinarizer
+import os
+import inspect
 
-
+current_dir = os.path.dirname(inspect.stack()[0][1])
 WORD2WEC_EMBEDDING_FILE = '/root/input/GoogleNews-vectors-negative300.bin.gz'
+PREMISE_FILE = current_dir + "/premise_indicator.txt"
+CLAIM_FILE = current_dir + "/claim_indicator.txt"
 
 def get_propositions(dataset):
     """Parse propositions
@@ -53,4 +65,180 @@ def add_word_vector_feature(dataset, propositionSet, parsedPropositions):
     dataset = pd.merge(dataset, wordVectorFrame, on='arg2')
 
     return dataset
+
     
+def add_pos_feature(dataset, propositionSet, parsedPropositions):
+    """Add Part-of-Speech features for every proposition"""
+
+    tagdict = nltk.data.load('help/tagsets/upenn_tagset.pickle')
+    lb = LabelBinarizer()
+    lb.fit(list(tagdict.keys()))
+    
+    propositionPOSList = list()
+
+    current = 0
+    for proposition in parsedPropositions:
+
+        propositionPOS = self.get_one_hot_pos(proposition, lb)
+        propositionPOSList.append(propositionPOS)
+
+    propositionPOSPadded = kp_pad_sequences(propositionPOSList, value=0, padding='post')
+
+    posFrame = pd.DataFrame({"arg1":propositionSet, "pos1": propositionPOSPadded.tolist()})
+    dataset = pd.merge(dataset, posFrame, on='arg1')
+    posFrame = posFrame.rename(columns={'arg1':'arg2', "pos1":"pos2"})
+    dataset = pd.merge(dataset, posFrame, on='arg2')
+
+    return dataset
+
+    
+def get_one_hot_pos(parsedProposition, label_binarizer):
+    """Get one-hot encoded PoS for the proposition"""
+
+    posVectorList = label_binarizer.transform([word[1] for word in parsedProposition])
+    posVector = np.array(posVectorList)
+
+    return posVector
+
+
+def add_keyword_feature(dataset, propositionSet):
+    """Add premise and claim flag for every proposition"""
+
+    premise_list = read_key_words((current_dir + PREMISE_FILE))
+    claim_list = read_key_words((current_dir + CLAIM_FILE))
+
+    keyWordFeatureList = list()
+
+    for proposition in propositionSet:
+
+        originalSentence = dataset.loc[dataset['arg1'] == proposition]['originalArg1'].iloc[0]
+        keyWordFeatureList.append(including_keywords_features(proposition, originalSentence, premise_list, claim_list))
+
+    keywordFeatureFrame = pd.DataFrame(data=keyWordFeatureList, columns=["claimIndicatorArg1", "premiseIndicatorArg1"])
+    keywordFeatureFrame["arg1"] = propositionSet
+
+    dataset = pd.merge(dataset, keywordFeatureFrame, on='arg1')
+
+    keywordFeatureFrame = keywordFeatureFrame.rename(columns = {'arg1':'arg2', 'claimIndicatorArg1':'claimIndicatorArg2', 'premiseIndicatorArg1': 'premiseIndicatorArg2'})
+
+    return pd.merge(dataset, keywordFeatureFrame, on='arg2')
+
+
+def including_keywords_features(proposition, original,
+                                premise_list, claim_list):
+    """Check if the proposition is a keyword or part of a key phrase
+    proposition: to check if keyword
+    original: sentence
+    premise_list: list of premise keywords
+    claim_list: list of claim keywords
+    Return:
+        [premise, claim] - 1 if sentence contains keyword"""
+
+    positionInSentence = original.find(proposition)
+
+    if positionInSentence < 1:
+
+        claim_indicator = check_claim_indicators(original[:len(proposition)], claim_list)
+        premise_indicator = check_premise_indicators(original[:len(proposition)], premise_list)
+
+    else:
+
+        wordTokensBefore = nltk.tokenize.word_tokenize(original[:positionInSentence])
+
+        if len(wordTokensBefore) > 1:
+
+            wordsBefore = wordTokensBefore[-2] + wordTokensBefore[-1]
+
+        else:
+
+            wordsBefore = wordTokensBefore[-1]
+
+        extendedSentence = "".join(wordsBefore) + " " + proposition
+
+        claim_indicator = check_claim_indicators(extendedSentence, claim_list)
+        premise_indicator = check_premise_indicators(extendedSentence, premise_list)
+
+    return [claim_indicator, premise_indicator]
+
+    
+def check_premise_indicators(sentence, premise_list):
+    """
+    function to detect the presence of argument keywords in a sentence
+    :param full sentence:
+    :return: 1 if sentence contains keyword
+    """
+
+    for indicator in premise_list:
+        if re.search(r"\b" + re.escape(indicator) + r"\b", sentence):
+            return 1
+    return 0
+
+
+def check_claim_indicators(sentence, claim_list):
+    """
+    function to detect the presence of argument keywords in a sentence
+    :param full sentence:
+    :return: True if sentence contains keyword
+    """
+    for indicator in claim_list:
+        if re.search(r"\b" + re.escape(indicator) + r"\b", sentence):
+            return 1
+    return 0
+
+
+def read_key_words(file):
+    """Reads list of words in file, one keyword per line"""
+    return [line.rstrip('\n') for line in open(file)]
+
+
+def add_token_feature(dataset, propositionSet, parsedPropositions):
+    """Add list of propositions and length of propositions to dataset"""
+
+    numberOfTokens = list()
+
+    for i in range(len(propositionSet)):
+
+        numberOfTokens.append([propositionSet[i], len((parsedPropositions[i]))])
+
+    tokenDataFrame = pd.DataFrame(data=numberOfTokens, columns=["proposition", "tokens"])
+
+    tokenDataFrame = tokenDataFrame.rename(columns={'proposition':'arg1', 'tokens':'tokensArg1'})
+
+    dataset = pd.merge(dataset, tokenDataFrame, on='arg1')
+
+    tokenDataFrame = tokenDataFrame.rename(columns={"arg1" : "arg2", "tokensArg1":"tokensArg2"})
+
+    dataset = pd.merge(dataset, tokenDataFrame, on='arg2')
+
+    return dataset
+
+    
+def add_shared_noun_feature(dataset, propositionSet, parsedPropositions):
+    """Add binary has shared noun and number of shared nouns to the dataset"""
+        
+    temp = dataset[['arg1','arg2']].apply(lambda row: find_shared_nouns(parsedPropositions[propositionSet.index(row['arg1'])], parsedPropositions[propositionSet.index(row['arg2'])]), axis=1)
+    temp = pd.DataFrame(temp.tolist(), columns=['sharedNouns', 'numberOfSharedNouns'])
+    dataset["sharedNouns"] = temp.loc[:,'sharedNouns']
+    dataset["numberOfSharedNouns"] = temp.loc[:,'numberOfSharedNouns']
+        
+    return dataset
+
+
+def find_shared_nouns(proposition, partner):
+
+    arg1Nouns = [word for (word, pos) in proposition if pos == 'NN']
+    arg2Nouns = [word for (word, pos) in partner if pos == 'NN']
+    intersection = set(arg1Nouns).intersection(arg2Nouns)
+    shared = 0
+    if len(intersection)>0:
+        shared = 1
+    
+    return [shared, len(intersection)]
+
+
+def add_same_sentence_feature(dataset):
+    """Add binary feature true if the two argument has the same original sentence"""
+
+    dataset["sameSentence"] = dataset[['originalArg1','arg2']].apply(lambda row: int(bool(row['arg2'] in row['originalArg1'])), axis=1)
+
+    return dataset
